7e28b63d404ca48a44b647fd01444f077d42a2fc
Nazir <anwernazir674@gmail.com>
2023-11-07 01:28:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold text-green-800 '>Hello world</h1>
-    </div>
+    <Body/>
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <>
+      <div  >
+        <Header />
+        <div  >
+          <img
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+            alt="background image"
+            
+          />
+        </div>
+        <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+          <h1 className="font-bold text-3xl py-4">Sign In</h1>
+          <input
+            type="text"
+            placeholder="Email Adress or Mobile Number"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+          <button className="p-4 my-4 bg-red-700 w-full rounded-lg">Sign In</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
