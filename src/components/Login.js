1a675520a7af2233a79361ffff847610c83f1e35
Nazir <anwernazir674@gmail.com>
2023-11-14 23:20:18 +0530

    frontend

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
     <>
       <div  >
         <Header />
-        <div  >
+        <div className="absolute" >
           <img
             src="https://assets.nflxext.com/ffe/siteui/vlv3/ab4b0b22-2ddf-4d48-ae88-c201ae0267e2/0efe6360-4f6d-4b10-beb6-81e0762cfe81/IN-en-20231030-popsignuptwoweeks-perspective_alpha_website_large.jpg"
             alt="background image"
