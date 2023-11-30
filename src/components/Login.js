96194a1373ee08e204375df75fdc7286fae330a2
Nazir <anwernazir674@gmail.com>
2023-12-01 02:25:06 +0530

    form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,21 @@
-import React,{useState} from "react";
+import React,{useRef, useState} from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
 
   const [isSignInForm,setSignInForm]=useState(true);
+  const [errormsg,setErrorMsg]=useState(null);
 
+  const email = useRef(null);
+  const password=useRef(null);
+  
+  const handleClick =() =>{
+    // form data validation
+    const msg=checkValidData(email.current.value,password.current.value);
+    setErrorMsg(msg);
+  }
+  
   const toggleForm =()=>{
     setSignInForm(!isSignInForm);
   }
@@ -20,7 +31,7 @@ const Login = () => {
             
           />
         </div>
-        <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <form onSubmit={(e)=> e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
           <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In":"Sign Up"}</h1>
           {!isSignInForm && <input
             type="text"
@@ -28,16 +39,19 @@ const Login = () => {
             className="p-4 my-4 w-full bg-gray-700"
           />}
           <input
+            ref={email}
             type="text"
             placeholder="Email Adress or Mobile Number"
             className="p-4 my-4 w-full bg-gray-700"
           />
           <input
+            ref={password}
             type="password"
             placeholder="Password"
             className="p-4 my-4 w-full bg-gray-700"
           />
-          <button  className="p-4 my-4 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign In":"Sign Up"}</button>
+          <p className="font-bold text-red-600">{errormsg}</p>
+          <button onClick={handleClick} className="p-4 my-4 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign In":"Sign Up"}</button>
           <p onClick={toggleForm} className="p-4 cursor-pointer">{isSignInForm ? "New to Netflix? Sign Up Now":"Already Registered? Sign In Now"}</p>
         </form>
       </div>
